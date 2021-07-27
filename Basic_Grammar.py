3e59ef0efc0c0e06234417a9eecca5e4c9753396
Xsop-sop <70881925+Xsop-sop@users.noreply.github.com>
2021-07-27 19:12:09 +0900

    Add files via upload

---
+++ Basic_Grammar.py
@@ -0,0 +1,39 @@
+# 사칙연산
+2 + 3 # 2와 3 더하기
+
+print(2 + 3) # 2와 3을 더한 값 출력
+
+print(5 - 2) # 5에서 2를 뺀 값 출력
+
+print(3 * 2)
+print(9 / 3)
+
+# 변수에 숫자 대입하고 계산하기
+a = 2
+b = 3
+print(a + b)
+
+# 변수에 문자 대입하고 출력하기
+a = "변수에 문자 대입하고 출력하기"
+print(a)
+
+# 조건문 if
+a = 5
+if a > 3:
+    print("a 는 3보다 큽니다.")
+
+# 반복문 for
+for a in ['Python', '기초', '강의']:
+    print(a)
+
+# 반복문 while
+a = 0
+while a < 5:
+    a=a+1
+    print(a)
+
+# 함수 def
+def puls(a, b):
+    return a + b
+
+print(puls('Xsop', '블로그'))
