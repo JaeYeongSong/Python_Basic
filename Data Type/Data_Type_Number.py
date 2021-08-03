717bc6a7687f3ff9f4eea2ed4a039559c41c7c9e
Xsop-sop <70881925+Xsop-sop@users.noreply.github.com>
2021-08-03 20:47:27 +0900

    Add files via upload

---
+++ Data Type/Data_Type_Number.py
@@ -0,0 +1,61 @@
+# 정수형
+123 # 양의 정수
+-123 # 양의 음수
+0 # 0
+
+a = 123
+b = -123
+c = 0
+print(type(a))
+print(type(b))
+print(type(c))
+
+# 실수형
+1.5
+-2.75
+
+a = 1.5
+b = -2.75
+print(type(a))
+print(type(b))
+
+4.28E10
+4.24e-10
+
+a = 4.28E10
+b = 4.24e-10
+print(type(a))
+print(type(b))
+
+# 8진수와 16진수
+0o177
+
+0x8ff
+0xABC
+
+# 사칙연산
+print(2 + 3)
+print(5 - 3)
+print(7 * 8)
+print(9 / 3)
+
+a = 2 + 3
+b = 5 - 3
+c = 7 * 8
+d = 9 / 3
+print(a)
+print(b)
+print(c)
+print(d)
+
+# 제곱
+a = 2 ** 3
+print(a)
+
+# 나눗셈 후 나머지
+a = 10 % 3
+print(a)
+
+# 나눗셈 후 몫
+a = 5 // 3
+print(a)
