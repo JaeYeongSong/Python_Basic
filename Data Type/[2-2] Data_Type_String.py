f1594739b1dafec3e0a0073ef1c7fd70a100f74c
Xsop-sop <70881925+Xsop-sop@users.noreply.github.com>
2021-08-04 22:24:33 +0900

    Add files via upload

---
+++ Data Type/[2-2] Data_Type_String.py
@@ -0,0 +1,20 @@
+# 문자열 더해서 연결하기(String Concatenation)
+a = "Python 자료형"
+b = " - 문자열"
+print(a + b)
+
+# 문자열 곱해서 반복하기
+a = "Python"
+print(a * 5)
+
+# 문자열 곱하기 응용해보기
+print("-" * 30)
+print("10,000원")
+
+# 문자열 길이 구하기
+a = "Python 자료형 - 문자열"
+print(len(a))
+
+# 문자열 길이 구하기 - 띄워쓰기
+a = "          " # 10칸 띄우기
+print(len(a))
